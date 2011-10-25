36f84237cd1ac429f9f33b7885018789b6ddd1e3
henrywoo1978 <henrywoo1978@5f226dd0-81a2-2681-d06b-3a14c16224dc>
2011-10-25 02:38:18 +0000

    implemented the msgFlightAvailability_Result_List and now going to implement the listview for the travelAgentGUI

--- trunk/Jade_Project/src/message/msgFlightAvailability_Result.java
+++ trunk/Jade_Project/src/message/msgFlightAvailability_Result.java
@@ -19,43 +19,72 @@ public class msgFlightAvailability_Result implements Serializable {
     public msgFlightAvailability_Result(){
         
     }
-    
-    //default constructor
-    public msgFlightAvailability_Result(String airline, String flightID, Date departDate, Date arrivalDate, Double total, String origin_city, String des_city){
-        sAirliner.add(airline);
-        sFlightID.add(flightID);
-        dDepartureDate.add(departDate);
-        dArrivalTime.add(arrivalDate);
-        dAirFareTotal.add(total);
-        sOrigin_City.add(origin_city);
-        sDestination_City.add(des_city);
-    }
    
-    private ArrayList<String> sAirliner = new ArrayList<String>();
-    private ArrayList<String> sFlightID = new ArrayList<String>();
-    private ArrayList<Date> dDepartureDate = new ArrayList<Date>();
-    private ArrayList<Date> dArrivalTime = new ArrayList<Date>();
-    private ArrayList<Double> dAirFareTotal = new ArrayList<Double>();
-    private ArrayList<String> sOrigin_City = new ArrayList<String>();
-    private ArrayList<String> sDestination_City = new ArrayList<String>();
-    
-    //get number of flights available
-    public int getCount(){
-        return sAirliner.size();
+    //default constructor
+    public msgFlightAvailability_Result(String airline, String flightno, Date departDate, Date arrivalDate, Double cost, String orgin_city, String des_city){
+        setAirliner(airline);
+        setFlightID(flightno);
+        setDepartureDate(departDate);
+        setArrivalDate(arrivalDate);
+        setAirFareTotal(cost);
+        setDeparture_City(orgin_city);
+        setDestination_City(des_city);
     }
     
-    //to get all the flights that is according to user's specification
-    public msgFlightAvailability_Result getFlightsAccordingToSpecs(msgReqFlightAvailability input){
-        msgFlightAvailability_Result result = new msgFlightAvailability_Result();
-        
-        //if found the right flight, then add to the list
-        for(int i = 0; i < sAirliner.size(); i++){
-            if(dDepartureDate.get(i).compareTo(input.getOnWard_Date()) == 0 && dAirFareTotal.get(i) <= input.getBudget() && sOrigin_City.get(i).compareTo(input.getOrigin_City()) == 0 && sDestination_City.get(i).compareTo(input.getDestination_City()) == 0){
-                result = new msgFlightAvailability_Result(sAirliner.get(i), sFlightID.get(i), dDepartureDate.get(i), dArrivalTime.get(i), dAirFareTotal.get(i), sOrigin_City.get(i), sDestination_City.get(i));
-            }
-        }
-        
-        return result;
+    String sAirliner;
+    public String getAirliner(){
+        return this.sAirliner;
+    }
+    public void setAirliner(String sValue){
+        this.sAirliner = sValue;
+    }
+    
+    private String sFlightID;
+    public String getFlightID(){
+        return this.sFlightID;
+    }
+    public void setFlightID(String sValue){
+        this.sFlightID = sValue;
+    }
+    
+    private Date dDepartureDate;
+    public Date getDepartureDate(){
+        return this.dDepartureDate;
+    }
+    public void setDepartureDate(Date dValue){
+        this.dDepartureDate = dValue;
+    }
+    
+    private Date dArrivalDate;
+    public Date getArrivalDate(){
+        return this.dDepartureDate;
+    }
+    public void setArrivalDate(Date dValue){
+        this.dArrivalDate = dValue;
+    }
+    
+    private Double dAirFareTotal;
+    public Double getAirFareTotal(){
+        return this.dAirFareTotal;
+    }
+    public void setAirFareTotal(Double dValue){
+        this.dAirFareTotal = dValue;
+    }
+    
+    private String sDeparture_City;
+    public String getDeparture_City(){
+        return this.sDeparture_City;
+    }
+    public void setDeparture_City(String sValue){
+        this.sDeparture_City = sValue;
+    }
+    
+    private String sDestination_City;
+    public String getDestination_City(){
+        return this.sDestination_City;
+    }
+    public void setDestination_City(String sValue){
+        this.sDestination_City = sValue;
     }
     
 }
