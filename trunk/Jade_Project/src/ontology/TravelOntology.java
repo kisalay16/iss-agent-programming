d0db616c8cc9c96f01d8c81071f3e5a95efd6d64
henrywoo1978 <henrywoo1978@5f226dd0-81a2-2681-d06b-3a14c16224dc>
2011-10-29 02:31:42 +0000

    added travelOntology

---
+++ trunk/Jade_Project/src/ontology/TravelOntology.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ontology;
+
+import java.util.Date;
+
+/**
+ *
+ * @author henry
+ */
+public class TravelOntology {
+    public static final String NAME = "travel-ontology";
+
+    // VOCABULARY
+    // Concepts
+    public static final String ORIGIN_CITY = "origin_city";
+    public static final String ORIGIN_COUNTRY = "origin_country";
+    public static final String DESTINATION_CITY = "destination_city";
+    public static final String DESTINATION_COUNTRY = "origin_country";
+    public static final String ONWARD_DATE = "onward_date";
+    public static final String RETURN_DATE = "return_date";
+    public static final String NO_OF_TRAVELLERS = "no_of_traveller";
+    public static final String BUDGET = "budget";
+    
+}
